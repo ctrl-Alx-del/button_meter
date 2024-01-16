c980975b92738ec44b11c268f1a0cf264cbce596
Alexander <electromanati@gmail.com>
2024-01-16 13:56:53 +0100

    refactored code. It now works as intended

--- script.js
+++ script.js
@@ -1,73 +1,60 @@
+//initiates the functions when the document is loaded
 document.addEventListener("DOMContentLoaded", init);
 
 //variables
 const btnIncrease = document.querySelector("button");
 const progressBar = document.querySelector("#block");
 const percentage = document.querySelector("#percentage");
+
 let status_ = false;
 let interval = null;
-let incFlag = false;
-let incrementIntervalActive = false;
+let progressNum = 0;
 
 function init() {
-  btnChangeColor();
   barIncrease();
+  btnChangeColor(progressNum);
 }
 
 function barIncrease() {
-  let progressNum = 0;
-
-  //Do so the bar moves linearly upwards instead of jumping WITHOUT css animation!
+  //mouse event-listeners
   btnIncrease.addEventListener("mousedown", () => {
     status_ = true;
-    incFlag = true;
-
-    if (!incrementIntervalActive) {
-      incrementIntervalActive = true;
-      update();
-    }
+    update();
   });
 
   btnIncrease.addEventListener("mouseup", () => {
     status_ = false;
-    incFlag = false;
     update();
   });
 
   function update() {
-    if (status_ === true) {
-      interval = setInterval(() => {
-        if (progressNum <= 99) {
-          progressNum++;
-          console.log("progress Num: ", progressNum);
-        }
+    //clears interval every time update is run. So it won't run infinitely
+    clearInterval(interval);
 
-        progressBar.style.height = `${progressNum}px`;
-        percentage.textContent = `${progressNum} %`;
-      }, 30);
-    } else {
-      clearInterval(interval);
+    //sets an interval so that the bar will progress towards hundred and decrease if mouse isn't held down
+    interval = setInterval(() => {
+      if (status_ && progressNum <= 99) {
+        progressNum++;
+      } else if (!status_ && progressNum > 0) {
+        progressNum--;
+      } else if (progressNum === 100) {
+        //call another function that changes something
+      }
 
-      interval = setInterval(() => {
-        if (!incFlag && progressNum > 0) {
-          progressNum--;
-          console.log("progress Num: ", progressNum);
-        } else if (progressNum === 0) {
-          clearInterval(interval);
-          incrementIntervalActive = false;
-        }
-
-        progressBar.style.height = `${progressNum}px`;
-        percentage.textContent = `${progressNum} %`;
-      }, 30);
-    }
+      //UI stuff
+      progressBar.style.height = `${progressNum}px`;
+      percentage.textContent = `${progressNum} %`;
+    }, 30);
   }
 }
 
-function btnChangeColor() {
+function btnChangeColor(num) {
   //make the color of the button change as you hold it in, in rythm with the progressbar
-  let r = 255;
+  let r = 0;
   let g = 0;
+  let b = 0;
+
+  //convert progressionNum into 255 rgb so the black hole will light up as the bar fills
   btnIncrease.addEventListener("mousedown", () => {
     //Does so the color doesn't exceed the given rgb ranges of 0 or 255. For both green and red
     if (r === 0) {
